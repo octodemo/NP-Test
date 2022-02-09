2ee502cc11e7d9e9fd622d2a46da2e736420198b
Niels Pineda <nielspineda@github.com>
2022-02-08 17:23:57 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log("Hello world!");
 let userName = "GH0811JLH"
 let myPassword = "Te5Tp@sw"
+let var = "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"
